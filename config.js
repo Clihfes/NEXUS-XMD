52b2a1a9850391996a457b8ea31cd2ad2f88f41c
Clihfes <154602009+Clihfes@users.noreply.github.com>
2025-04-29 03:52:41 +0300

    .

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "PkdrillerwDkzRSAC#1eBHSn2hP5AVqo-psaxnUw2I9KtwmxCHmeZBidHqlig",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
